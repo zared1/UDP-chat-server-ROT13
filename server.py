cb35d93954d091e327bf39b4258a6ab6a0450b9e
Zakarye R. <168565323+zared1@users.noreply.github.com>
2025-10-19 22:03:40 +0100

    Update server.py

--- server.py
+++ server.py
@@ -2,28 +2,28 @@ import socket
 import string
 
 def rot13(msg):
-    trans_table = msg.maketrans(string.ascii_lowercase + string.ascii_uppercase,
-                                    string.ascii_lowercase[13:] + string.ascii_lowercase[:13] +
-                                    string.ascii_uppercase[13:] + string.ascii_uppercase[:13])
-
-    enc_msg = message.translate(trans_table)
-
+    trans_table = msg.maketrans(
+        string.ascii_lowercase + string.ascii_uppercase,
+        string.ascii_lowercase[13:] + string.ascii_lowercase[:13] +
+        string.ascii_uppercase[13:] + string.ascii_uppercase[:13]
+    )
+    enc_msg = msg.translate(trans_table)
     return enc_msg
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(('', 10001))
 
-    print("CommServer initiated on port 10001. Prepare for incoming enemies.")
+    print("UDP ROT13 Chat Server initiated on port 10001. Ready for incoming messages.")
 
     while True:
         data, addr = sock.recvfrom(1024)
-        msg data.decode()
-        print(f"Discovered a transmission from {addr}: {msg}")
-        
-        response = input("Speak now:)
+        msg = data.decode()
+        print(f"Received from {addr}: {msg}")
+
+        response = input("Server response: ")
         enc_response = rot13(response)
-        sock.sendto(encoded_response.encode(), addr)
+        sock.sendto(enc_response.encode(), addr)
 
 if __name__ == '__main__':
     main()
